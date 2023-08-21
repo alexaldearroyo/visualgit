9a9781b150a97159698d911c5359b61bee527450
alexaldearroyo <alexaldearroyo@gmail.com>
2023-08-21 10:21:28 +0900

    Local commit test

--- visualgit.py
+++ visualgit.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+import sys
+
 
 def is_git_installed():
     try:
@@ -9,8 +12,10 @@ def is_git_installed():
         pass
     return False
 
+
 print("\nVISUAL GIT")
 print("-"*30)
+
 
 def main():
     if not is_git_installed():
@@ -22,7 +27,7 @@ def main():
         print("[b] Work in branches")
         print("[l] Check log")
         print("[c] Configuration")
-        print("[x] Exit\n")
+        print("[q] Quit program\n")
 
         choice = input("Please select an option: ")
 
@@ -34,11 +39,12 @@ def main():
             check_log()
         elif choice == "c":
             configuration()
-        elif choice == "x":
+        elif choice == "q":
             print("Exiting VisualGit...\n")
             break
         else:
             print("Invalid choice. Please select a valid option.")
+
 
 def work_in_main():
     while True:
@@ -48,6 +54,7 @@ def work_in_main():
         print("[rl] Remote to local")
         print("[m] Manage repos")
         print("[x] Back to main menu")
+        print("[q] Quit program")
 
         choice = input("\nPlease select an option: ")
 
@@ -61,12 +68,66 @@ def work_in_main():
             main_manage_repos()
         elif choice == "x":
             break
+        elif choice == "q":
+            sys.exit("Exiting VisualGit...\n")
         else:
             print("Invalid choice. Please select a valid option")
 
-
 def main_local():
-    print("Working locally...")
+    while True:
+        print("\nMain -Local:")
+        print("[c] Check local repos")
+        print("[a] Create a local repo")
+        print("[m] Commit to local repo")
+        print("[x] Back to previous menu")
+        print("[q] Quit program")
+
+        choice = input("\nPlease select an option: ")
+
+        if choice == "c":
+            check_local_repos()
+        elif choice == "a":
+            create_local_repo()
+        elif choice == "m":
+            commit_to_local_repo()
+        elif choice == "x":
+            break
+        elif choice == "q":
+            sys.exit("Exiting VisualGit...\n")
+        else:
+            print("Invalid choice. Please select a valid option")
+
+def check_local_repos():
+    if os.path.exists('.git'):
+        try:
+            subprocess.run(["git", "status"])
+        except Exception as e:
+            print(f"Error executing git status: {e}")
+    else:
+        print("The present working directory is not yet a git repository")
+
+def create_local_repo():
+    if os.path.exists('.git'):
+        print("The present working directory is already a git repository. No need to create one.")
+    else:
+        try:
+            subprocess.run(["git", "init"])
+            print("Local repository has been successfully created in the present working directory.")
+        except Exception as e:
+            print(f"Error while creating local repository: {e}")
+
+def commit_to_local_repo():
+    if not os.path.exists('.git'):
+        print("The present working directory is not a git repository. In order to commit you have to create a local repository.")
+        return
+    
+    try:
+        subprocess.run(["git", "add", "."])
+        message = input("Enter commit message: ")
+        subprocess.run(["git", "commit", "-m", message])
+
+    except Exception as e:
+        print(f"Error making commit: {e}")
 
 def main_local_to_remote():
     print("Transferring from local to remote...")
@@ -77,6 +138,7 @@ def main_remote_to_local():
 def main_manage_repos():
     print("Managing repositories...")
 
+
 def work_in_branches():
     while True:
         print("\nWorking in branches:")
@@ -84,6 +146,7 @@ def work_in_branches():
         print("[lr] Local to remote")
         print("[m] Manage branches")
         print("[x] Back to main menu")
+        print("[q] Quit program")
 
         choice = input("\nPlease select an option: ")
 
@@ -95,6 +158,8 @@ def work_in_branches():
             manage_branches()
         elif choice == "x":
             break
+        elif choice == "q":
+            sys.exit("Exiting VisualGit...\n")
         else:
             print("Invalid choice. Please select a valid option")
 

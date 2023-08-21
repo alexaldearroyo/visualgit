fb697f455fa02dd22abb493f5eed3c6eb7978472
alexaldearroyo <alexaldearroyo@gmail.com>
2023-08-21 11:29:43 +0900

    Local and local-to-remote functions implemented.

--- visualgit.py
+++ visualgit.py
@@ -11,6 +11,23 @@ def is_git_installed():
     except Exception as e:
         pass
     return False
+def is_git_repo():
+    return os.path.exists('.git')
+def print_git_repo():
+    print("The present working directory is already a git repository. No need to create one.")
+def print_not_git_repo():
+    print("The present working directory is not a git repository, please create one to proceed.")
+def is_connected_to_remote():
+    try:
+        result = subprocess.run(["git", "remote", "get-url", "origin"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.returncode == 0
+    except Exception:
+        return False
+def print_connected_to_remote():
+    print("The local repository is already connected to a remote repository.")
+def print_not_connected_to_remote():
+    print("The local repository is not connected to a remote repository. Please connect it to proceed.")
+    
 
 
 print("\nVISUAL GIT")
@@ -45,7 +62,7 @@ def main():
         else:
             print("Invalid choice. Please select a valid option.")
 
-
+# MAIN
 def work_in_main():
     while True:
         print("\nWork in main:")
@@ -73,6 +90,7 @@ def work_in_main():
         else:
             print("Invalid choice. Please select a valid option")
 
+# MAIN -LOCAL
 def main_local():
     while True:
         print("\nMain -Local:")
@@ -98,27 +116,30 @@ def main_local():
             print("Invalid choice. Please select a valid option")
 
 def check_local_repos():
-    if os.path.exists('.git'):
-        try:
-            subprocess.run(["git", "status"])
-        except Exception as e:
+    if not is_git_repo():
+        print_not_git_repo()
+        return
+    
+    try:
+        subprocess.run(["git", "status"])
+    except Exception as e:
             print(f"Error executing git status: {e}")
-    else:
-        print("The present working directory is not yet a git repository")
+ 
 
 def create_local_repo():
-    if os.path.exists('.git'):
-        print("The present working directory is already a git repository. No need to create one.")
-    else:
-        try:
-            subprocess.run(["git", "init"])
-            print("Local repository has been successfully created in the present working directory.")
-        except Exception as e:
-            print(f"Error while creating local repository: {e}")
+    if is_git_repo():
+        print_git_repo()
+        return
+    
+    try:
+        subprocess.run(["git", "init"])
+        print("Local repository has been successfully created in the present working directory.")
+    except Exception as e:
+        print(f"Error while creating local repository: {e}")
 
 def commit_to_local_repo():
-    if not os.path.exists('.git'):
-        print("The present working directory is not a git repository. In order to commit you have to create a local repository.")
+    if not is_git_repo():
+        print_not_git_repo()
         return
     
     try:
@@ -129,16 +150,104 @@ def commit_to_local_repo():
     except Exception as e:
         print(f"Error making commit: {e}")
 
+
+# MAIN LOCAL_TO_REMOTE
 def main_local_to_remote():
+    while True:
+        print("\nMain -Local to remote:")
+        print("[c] Check remote repos")
+        print("[l] Link local repo with remote")
+        print("[p] Push changes to remote")
+        print("[cp] Commit & Push")
+        print("[x] Back to previous menu")
+        print("[q] Quit program")
+
+        choice = input("\nPlease select an option: ")
+
+        if choice == "c":
+            check_remote_repos()
+        elif choice == "l":
+            connect_local_with_remote()
+        elif choice == "p":
+            push_changes_to_remote()
+        elif choice == "cp":
+            commit_and_push()
+        elif choice == "x":
+            break
+        elif choice == "q":
+            sys.exit("Exiting VisualGit...\n")
+        else:
+            print("Invalid choice. Please select a valid option")
+
     print("Transferring from local to remote...")
 
+def check_remote_repos():
+    try:
+        subprocess.run(["git", "remote", "-v"])
+    except Exception as e:
+        print(f"Error checking remote repository: {e}")
+
+def connect_local_with_remote():
+    if not is_git_repo():
+        print_not_git_repo()
+        return
+    
+    if is_connected_to_remote():
+        print_connected_to_remote()
+        return
+
+    remote_url = input("Enter the remote repository (GitHub) URL: ")
+    try:
+        subprocess.run(["git", "remote", "add", "origin", remote_url])
+        print(f"Connected local repository with remote: {remote_url}")
+    except Exception as e:
+        print(f"Error connecting with remote: {e}")
+
+def push_changes_to_remote():
+
+    if not is_git_repo():
+        print_not_git_repo()
+        return
+    
+    if not is_connected_to_remote():
+        print_not_connected_to_remote()
+        return
+    
+    try:
+        subprocess.run(["git", "push", "-u", "origin", "main"])
+    except Exception as e:
+        print(f"Error pushing changes to remote: {e}")
+
+def commit_and_push():
+
+    if not is_git_repo():
+        print_not_git_repo()
+        return
+    
+    if not is_connected_to_remote():
+        print_not_connected_to_remote()
+        return
+
+    try:
+        subprocess.run(["git", "add", "."])
+        message = input("Enter commit message: ")
+        subprocess.run(["git", "commit", "-m", message])
+        subprocess.run(["git", "push", "origin", "main"])
+    except Exception as e:
+        print(f"Error committing and pushing: {e}")
+
+
+# MAIN REMOTE_TO_LOCAL
 def main_remote_to_local():
     print("Transferring from remote to local...")
 
+
+# MAIN MANAGE_REPOS
 def main_manage_repos():
     print("Managing repositories...")
 
 
+# BRANCHES
 def work_in_branches():
     while True:
         print("\nWorking in branches:")
